cd1dfab74357203ae9d9dd9864032553bffffcc4
olha kovalisko <olha.kovalisko@knameit.com>
2023-07-23 21:25:43 +0300

    added logic for navigation menu

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+const hamburger = document.querySelector(".hamburger");
+const headerContainer = document.querySelector(".header__container");
 
-const hamburger = document.querySelector('.hamburger');
-const headerContainer = document.querySelector('.header__container');
+hamburger.addEventListener("click", () => {
+  headerContainer.classList.toggle("menu-open");
+});
 
-hamburger.addEventListener('click', () => {
-  headerContainer.classList.toggle('menu-open');
+const navItems = document.querySelectorAll(".nav a");
+
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    navItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+    item.classList.add("active");
+  });
 });
